acaf3e8db300906d14fe282869aab1950e42d71b
Jason Kulatunga <jk17@ualberta.ca>
2016-04-30 23:20:02 -0600

    name and group can be dynamically assigned.

--- config.sh
+++ config.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 PUID=${PUID:=15000}
+PUSER=${PUSER:=mediadepot}
 PGID=${PGID:=15000}
+PGROUP=${PGROUP:=mediadepot}
 
 #Create internal mediadepot user (which will be mapped to external user and used to run the process)
-addgroup -g $PUID depot  && adduser -D -u $PGID -G depot depot
+addgroup -g $PGID $PGROUP  && adduser -D -u $PUID -G $PGROUP $PUSER
